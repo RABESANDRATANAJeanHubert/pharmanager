9efd12c4f7e2d628ed39f95e3321f37e60f23c9a
jeanHubert <bdadajean@gmail.com>
2022-02-25 10:54:41 +0300

    renommer

--- back/src/main.ts
+++ back/src/main.ts
@@ -4,5 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  console.log('new connexion has been register');
+  console.log('my connexion does work finally');
 }
 bootstrap();
